3f53aa2b4eed15c945ec92294d370ed9851df1bc
tmtri3102 <tmtri310251@gmail.com>
2025-02-19 09:25:00 +0700

    remove wrong images uploaded, commit before pull dev

--- src/main/java/com/codegym/service/host/HostRequestService.java
+++ src/main/java/com/codegym/service/host/HostRequestService.java
@@ -1,34 +1,34 @@
-package com.codegym.service.host;
-
-import com.codegym.model.HostRequest;
-import com.codegym.repository.IHostRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.Optional;
-
-@Service
-public class HostRequestService implements IHostRequestService {
-    @Autowired
-    private IHostRepository hostRepository;
-
-    @Override
-    public Iterable<HostRequest> findAll() {
-        return hostRepository.findAll();
-    }
-
-    @Override
-    public Optional<HostRequest> findById(Long id) {
-        return hostRepository.findById(id);
-    }
-
-    @Override
-    public void save(HostRequest object) {
-        hostRepository.save(object);
-    }
-
-    @Override
-    public void deleteById(Long id) {
-        hostRepository.deleteById(id);
-    }
-}
+//package com.codegym.service.host;
+//
+//import com.codegym.model.HostRequest;
+//import com.codegym.repository.IHostRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.Optional;
+//
+//@Service
+//public class HostRequestService implements IHostRequestService {
+//    @Autowired
+//    private IHostRepository hostRepository;
+//
+//    @Override
+//    public Iterable<HostRequest> findAll() {
+//        return hostRepository.findAll();
+//    }
+//
+//    @Override
+//    public Optional<HostRequest> findById(Long id) {
+//        return hostRepository.findById(id);
+//    }
+//
+//    @Override
+//    public void save(HostRequest object) {
+//        hostRepository.save(object);
+//    }
+//
+//    @Override
+//    public void deleteById(Long id) {
+//        hostRepository.deleteById(id);
+//    }
+//}
--- src/main/java/com/codegym/service/host/IHostRequestService.java
+++ src/main/java/com/codegym/service/host/IHostRequestService.java
@@ -1,7 +1,7 @@
 package com.codegym.service.host;
 
-import com.codegym.model.HostRequest;
+//import com.codegym.model.HostRequest;
 import com.codegym.service.IGenerateService;
 
-public interface IHostRequestService extends IGenerateService<HostRequest> {
-}
+//public interface IHostRequestService extends IGenerateService<HostRequest> {
+//}
